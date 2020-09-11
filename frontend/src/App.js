0d833b375b2a05a3b580a46b1f4765d6ad82f976
Ovy95 <jackoverton21@hotmail.com>
2020-09-11 17:29:02 +0100

    react component added for improving design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import './App.css';
 
 import { connect, sendMsg } from "./api";
-
+import Header from './components/Header/Header'
 class App extends Component {
   constructor(props) {
     super(props);
@@ -17,6 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <button onClick={this.send}>Hit</button>
       </div>
     );
